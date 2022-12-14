f7aa173bdc29abed0a9da3a64f8960ede7c904a2
Eric Sans <esansvi@gmail.com>
2022-12-14 01:56:49 +0100

    Eric Sans Vidal - Half of the MyPolynomial exercise

---
+++ src/MainPoly.java
@@ -0,0 +1,21 @@
+public class MainPoly {
+    public static void main(String[] args) {
+        MyPolynomial p1= new MyPolynomial(1,2,3);
+        /* It's possible to invoke with array:
+        double [] coeffs = {1.1,2.4,5.2,5.7};
+        MyPolynomial p2 = new MyPolynomial (coeffs);
+        * */
+
+        MyPolynomial p2 = new MyPolynomial("poly.txt");
+        System.out.println(p1.toString());
+        //System.out.println(p2);
+        //System.out.println(p1.getDegree());
+        //System.out.println(p1.evaluate(2));
+        MyPolynomial p3= new MyPolynomial(1,3,2);
+        System.out.println(p3.toString());
+        //MyPolynomial p4= new MyPolynomial(5);
+        //System.out.println(p4);
+        System.out.println(p3.add(p1));
+
+    }
+}
---
+++ src/MyPolynomial.java
@@ -0,0 +1,116 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MyPolynomial {
+
+    private double [] coeffs;
+    private int degree;
+
+    public MyPolynomial(double... coeffs){
+        this.coeffs = coeffs;
+        this.degree= coeffs.length-1;
+    }
+
+    public MyPolynomial(int degree){
+        this.degree= degree;
+        coeffs = new double[degree+1];
+        for (int i=0; i<degree; i++){
+            this.coeffs[i]=0;
+        }
+    }
+
+    public MyPolynomial(String filename){
+        Scanner in = null;
+            try {
+                in = new Scanner(new File(filename));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            this.degree = in.nextInt();
+            coeffs = new double[degree+1];
+            for (int i=0; i<coeffs.length; i++){
+                coeffs[i]=in.nextDouble();
+            }
+
+    }
+    public int getDegree() {
+        return degree;
+    }
+
+    public String toString() {
+        for (int i=this.degree; i>=1;i--){
+            System.out.print(this.coeffs[i]+"·x^"+(i)+"+");
+        }
+        System.out.print(this.coeffs[0]);
+        System.out.println();
+        return "";
+    }
+    public double evaluate(double x){
+        double evaluation=0;
+        for (int i=this.degree; i>=1;i--){
+            evaluation += this.coeffs[i]*Math.pow(x,i);
+        }
+        evaluation += this.coeffs[0];
+        return evaluation;
+    }
+
+    public MyPolynomial add(MyPolynomial another) {
+        MyPolynomial addition;
+        int maxDegree;
+        if (this.degree>= another.getDegree()){
+            maxDegree =this.degree;
+        } else {
+            maxDegree=another.getDegree();
+        }
+        addition = new MyPolynomial(maxDegree);
+        for (int h=maxDegree; h>=0;h--){
+            addition.coeffs[h] = this.coeffs[h]+ another.getCoeffs(h);
+        }
+        return addition;
+    }
+
+    public double getCoeffs(int n) {
+        return coeffs[n];
+    }
+}
+
+/*     public MyPolynomial add(MyPolynomial another) {
+        MyPolynomial addition = null;
+        int newDegree, i = 0;
+        if (this.degree == another.getDegree()) {
+            newDegree = this.degree;
+            addition = new MyPolynomial(newDegree);
+            for (int h = newDegree; h >= 0; h--) {
+                addition.coeffs[h] = this.coeffs[h] + another.coeffs[h];
+            }
+        } else if (this.degree > another.getDegree()) {
+            newDegree = this.degree;
+            MyPolynomial aux = new MyPolynomial(newDegree);
+            for (i=0; i< another.getDegree();i++){
+                aux.coeffs[i]=another.coeffs[i];
+        }
+            for (int j=i;j<newDegree;j++){
+                aux.coeffs[j]=0;
+            }
+            MyPolynomial addition = new MyPolynomial(newDegree);
+            for (int h=newDegree; h>=0;h--){
+                addition.coeffs[h] = this.coeffs[h]+aux.coeffs[h];
+            }
+
+        } else {
+            newDegree = another.getDegree();
+            MyPolynomial aux = new MyPolynomial(newDegree);
+            for (i=0; i< this.degree;i++){
+                aux.coeffs[i]=this.coeffs[i];
+            }
+            for (int j=i;j<newDegree;j++){
+                aux.coeffs[j]=0;
+            }
+
+            for (int h=newDegree; h>=0;h--){
+                addition.coeffs[h] = aux.coeffs[h]+another.coeffs[h];
+            }
+        }
+*/
